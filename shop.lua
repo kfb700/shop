a1b101980d783cfb351fc814ca9d0af9163e3ae4
kfb700 <78882289+kfb700@users.noreply.github.com>
2025-05-02 16:45:50 +0300

    1

--- shop.lua
+++ shop.lua
@@ -231,7 +231,7 @@ function createAutorizationForm()
     AutorizationForm:addLabel(22, 20, "███████║██║  ██║╚██████╔╝██║             ")
     AutorizationForm:addLabel(22, 21, "╚══════╝╚═╝  ╚═╝ ╚═════╝ ╚═╝             ")
     
-    AutorizationForm:addLabel(22, 23, "     ↓  Встаньте на PIM 5   ↓       ")
+    AutorizationForm:addLabel(22, 23, "     ↓  Встаньте на PIM 6   ↓       ")
     authorLabel.fontColor = 0x00FDFF
 
     return AutorizationForm

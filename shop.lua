e1affd0a26107bf2582972583f268cf0bcc8d514
kfb700 <78882289+kfb700@users.noreply.github.com>
2025-05-02 06:14:11 +0300

    12321312321

--- shop.lua
+++ shop.lua
@@ -185,7 +185,7 @@ function createNumberEditForm(callback, form, buttonText, pricePerItem, currentB
     end)
 
     local acceptButton = itemCounterNumberForm:addButton(17, showCalculation and 10 or 8, buttonText, function()
-        callback(itemCountEdit.text and tonumber(itemCountEdit.text) or 0
+        callback((itemCountEdit.text and tonumber(itemCountEdit.text)) or 0)
     end)
     
     if showCalculation then
